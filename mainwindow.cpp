5bad92d5aa6a3166ee1ccca10e3a11380ab4bea6
Anshul Gupta <anshulgupta0803@gmail.com>
2017-08-20 17:35:04 +0530

    Basic print syntax

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPrinter>
+#include <QPainter>
 #include <QCPDialog.h>
+#include <QPrintDialog>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
@@ -15,8 +17,30 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QCPDialog dialog(new QPrinter, Q_NULLPTR);
-    if (dialog.exec() != QDialog::Accepted)
+    QPrinter *printer = new QPrinter(QPrinter::HighResolution);
+
+    QCPDialog dialog(printer, Q_NULLPTR);
+//    QPrintDialog dialog(printer, Q_NULLPTR);
+
+    dialog.setWindowTitle("Print Document");
+
+    if (dialog.exec() != QDialog::Accepted) {
+        ui->label->setText("Print Cancelled");
         return;
-    ui->label->setText(dialog.information());
+    }
+
+    QPainter painter;
+    painter.begin(printer);
+
+    QRect rect({100, 100}, QSize{500, 500});
+    QString text{"Hello World"};
+
+    painter.drawText(rect, Qt::AlignLeft | Qt::AlignTop, text);
+    painter.drawEllipse(rect);
+
+    painter.end();
+
+    ui->label->setText("Print Successful");
+
+    return;
 }
